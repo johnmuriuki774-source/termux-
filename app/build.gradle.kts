35a7a3bb4398a3a353893ed4d886d12244fcf2e8
johnmuriuki774-source <johnmuriuki774@gmail.com>
2026-08-13 14:19:06 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("com.google.devtools.ksp")
 }
 
 android {
@@ -13,11 +11,13 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0"
     }
 
-    buildFeatures {
-        compose = true
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
     }
 
     compileOptions {
@@ -27,54 +27,13 @@ android {
 }
 
 dependencies {
-    val composeBom = platform(
-        "androidx.compose:compose-bom:2026.06.00"
-    )
-
-    implementation(composeBom)
-
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.activity:activity-compose:1.12.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.ui:ui:1.8.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
+    implementation("androidx.compose.material3:material3:1.3.2")
 
-    implementation(
-        "androidx.lifecycle:lifecycle-runtime-compose:2.9.2"
-    )
-
-    implementation(
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
-    )
-
-    implementation(
-        "com.squareup.okhttp3:okhttp:5.1.0"
-    )
-
-    implementation(
-        "io.coil-kt.coil3:coil-compose:3.3.0"
-    )
-
-    implementation(
-        "io.coil-kt.coil3:coil-network-okhttp:3.3.0"
-    )
-
-    // Room
-    implementation(
-        "androidx.room:room-runtime:2.7.2"
-    )
-
-    implementation(
-        "androidx.room:room-ktx:2.7.2"
-    )
-
-    ksp(
-        "androidx.room:room-compiler:2.7.2"
-    )
-
-    debugImplementation(
-        "androidx.compose.ui:ui-tooling"
-    )
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
 }
