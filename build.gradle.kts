5462e8f59a2a3dfb658575cf33f3ad5649e90aea
johnmuriuki774-source <johnmuriuki774@gmail.com>
2026-08-13 13:36:04 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "9.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.10-2.0.6" apply false
+    id("com.google.devtools.ksp") version "2.3.10" apply false
 }
